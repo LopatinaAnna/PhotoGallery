1562f1dcf396a97c79847ec0a6458106681b434e
Anna Lopatina <an.lopatina050@gmail.com>
2020-12-27 19:28:35 +0200

    Add login component

---
+++ PhotoGallery.Client/PhotoGallery/src/app/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+loginForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      'username': [''],
+      'password': ['']
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.loginForm.value);
+  }
+}
