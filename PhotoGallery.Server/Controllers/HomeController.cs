ab263746e5afc0e06b66703f8b08cb20e9f7f0dc
Anna Lopatina <an.lopatina050@gmail.com>
2020-12-27 16:36:54 +0200

    Добавьте файлы проекта.

---
+++ PhotoGallery.Server/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoGallery.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        //[Authorize]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+    }
+}
