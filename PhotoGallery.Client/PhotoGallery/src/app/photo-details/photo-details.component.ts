e6d507b119964bb5d77fd8dd72271d1514a01651
Anna Lopatina <an.lopatina050@gmail.com>
2021-01-18 19:23:04 +0200

    Client - Fix subscriptions with pipe and map

--- PhotoGallery.Client/PhotoGallery/src/app/photo-details/photo-details.component.ts
+++ PhotoGallery.Client/PhotoGallery/src/app/photo-details/photo-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { map, mergeMap } from 'rxjs/operators';
 import { Photo } from '../models/Photo';
 import { PhotoService } from '../services/photo.service';
 
@@ -9,16 +10,19 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['./photo-details.component.css']
 })
 export class PhotoDetailsComponent implements OnInit {
-  id: number = 0;
   photo: Photo;
 
   constructor(private route: ActivatedRoute, private photoService: PhotoService) {
-    this.route.params.subscribe(res => {
-      this.id = res['id'];
-      this.photoService.getPhoto(this.id).subscribe(res => {
-        this.photo = res;
-      })
-    })
+    this.fetchData();
+  }
+
+  fetchData(){
+    this.route.params.pipe(map(params => {
+      const id = params['id'];
+      return id;
+    }), mergeMap(id => this.photoService.getPhoto(id))).subscribe(res => {
+      this.photo = res;
+    });
   }
 
   ngOnInit() {
